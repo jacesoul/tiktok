7724ff0dff74d8bcb65300e8179d9fc7d51d7389
jaceseol <seolju90@gmail.com>
2023-07-31 18:02:03 +0900

    Stateful Navigation part One

---
+++ lib/features/main_navigation/stf_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class StfScreen extends StatefulWidget {
+  const StfScreen({super.key});
+
+  @override
+  State<StfScreen> createState() => _StfScreenState();
+}
+
+class _StfScreenState extends State<StfScreen> {
+  int _clicks = 0;
+
+  void _increase() {
+    setState(
+      () {
+        _clicks = _clicks + 1;
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print("");
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "$_clicks",
+          style: const TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        TextButton(
+          onPressed: _increase,
+          child: const Text("+"),
+        )
+      ],
+    );
+  }
+}
