b0582d84f66aa5574ac397aaee668b4fdb913a16
jacesoul <seolju90@gmail.com>
2023-10-09 23:07:13 +0900

    RotationTransition

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -11,22 +11,62 @@ class ActivityScreen extends StatefulWidget {
   State<ActivityScreen> createState() => _ActivityScreenState();
 }
 
-class _ActivityScreenState extends State<ActivityScreen> {
+class _ActivityScreenState extends State<ActivityScreen>
+    with SingleTickerProviderStateMixin {
+  // Ticker는 모든 애니메이션을 프레임에서 Callback Fuction 호출하는 시계 같은거다.
+  // SingleTickerProviderStateMixin은 Ticker를 주는 Minxin일 뿐만 아니라 위젯이 widget tree에 없을때 resource를 낭비하고 있지 않게 하기도 한다.
   final List<String> _notifications = List.generate(
     20,
     (index) => "${index}h",
   );
+
+// late modifier에서는 초기화문에서도 this를 참조할수 있다.
+// this나 다른 instance member를 참조하려면 late를 사용해야만 한다.
+// 이렇게 하는 경우 initState가 필요없다.
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 300),
+  );
+
+  late final Animation<double> _animation = Tween(
+    begin: 0.0,
+    end: 0.5,
+  ).animate(_animationController);
 
   void _onDismissed(String notification) {
     _notifications.remove(notification);
     setState(() {});
   }
 
+  void _onTitleTap() {
+    if (_animationController.isCompleted) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("All Activity"),
+        title: GestureDetector(
+          onTap: _onTitleTap,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("All Activity"),
+              Gaps.h2,
+              RotationTransition(
+                turns: _animation,
+                child: const FaIcon(
+                  FontAwesomeIcons.chevronDown,
+                  size: Sizes.size14,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
       body: ListView(
         children: [
