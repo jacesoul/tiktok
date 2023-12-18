814034f5b4fc7314c290b76baec8a009bd5cb353
jacesoul <seolju90@gmail.com>
2023-12-18 17:13:05 +0900

    likeVideo

--- lib/features/videos/repos/videos_repo.dart
+++ lib/features/videos/repos/videos_repo.dart
@@ -39,10 +39,14 @@ class VideosRepository {
     required String videoId,
     required String userId,
   }) async {
-    await _db.collection("likes").add({
-      "videoId": videoId,
-      "userId": userId,
-    });
+    final query = _db.collection("likes").doc("${videoId}000$userId");
+    final like = await query.get();
+
+    if (!like.exists) {
+      await query.set({
+        "createdAt": DateTime.now().millisecondsSinceEpoch,
+      });
+    }
   }
 }
 
